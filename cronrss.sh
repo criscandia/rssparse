504bee022de80a564594f72d85c775542f06c5db
Cristian Candia <candiacristian@gmail.com>
2016-03-16 14:27:08 -0300

    Agrego sitios

--- cronrss.sh
+++ cronrss.sh
@@ -7,9 +7,14 @@ source bin/activate
 
 #Execute script rssparse
 
-python ~/Dropbox/rssparse/rssparse/rssparser.py -f http://python.org.ar/trabajo/rss
-python ~/Dropbox/rssparse/rssparse/rssparser.py -f https://remoteok.io/remote-jobs.rss
-python ~/Dropbox/rssparse/rssparse/rssparser.py -f https://weworkremotely.com/categories/1/jobs.rss
-python ~/Dropbox/rssparse/rssparse/rssparser.py -f https://dribbble.com/jobs.rss?location=Anywhere
-python ~/Dropbox/rssparse/rssparse/rssparser.py -f https://jobspresso.co/?feed=job_feed&job_types=developer&search_location&job_categories&search_keywords
-#python ~/Dropbox/rssparse/rssparse/rssparser.py -f 
+python ~/Dropbox/rssparse/rssparse/RssparserNew.py -f http://python.org.ar/trabajo/rss
+python ~/Dropbox/rssparse/rssparse/RssparserNew.py -f https://remoteok.io/remote-jobs.rss
+python ~/Dropbox/rssparse/rssparse/RssparserNew.py -f https://weworkremotely.com/categories/1/jobs.rss
+python ~/Dropbox/rssparse/rssparse/RssparserNew.py -f https://dribbble.com/jobs.rss?location=Anywhere
+python ~/Dropbox/rssparse/rssparse/RssparserNew.py -f https://jobspresso.co/?feed=job_feed&job_types=developer&search_location&job_categories&search_keywords
+python ~/Dropbox/rssparse/rssparse/RssparserNew.py -f https://weworkremotely.com/categories/2-programming/jobs.rss
+python ~/Dropbox/rssparse/rssparse/RssparserNew.py -f https://jobs.github.com/positions.atom
+python ~/Dropbox/rssparse/rssparse/RssparserNew.py -f http://feeds.feedblitz.com/flexjobs
+python ~/Dropbox/rssparse/rssparse/RssparserNew.py -f http://careers.stackoverflow.com/jobs/feed?allowsremote=True
+
+
