13973eed61748345db7b55db55b3a6808554303f
Cristian Candia <candiacristian@gmail.com>
2022-12-20 20:30:43 -0300

    Django Jobs added

--- cronrss.sh
+++ cronrss.sh
@@ -2,19 +2,31 @@
 
 #Activate virtualenv
 
-cd ~/virtualenv/rssparse
-source bin/activate
+cd ~/Documents/Programs/Projects/rssparse
+
+source env/bin/activate
 
 #Execute script rssparse
 
-python ~/Dropbox/rssparse/rssparse/RssparserNew.py -f http://python.org.ar/trabajo/rss
-python ~/Dropbox/rssparse/rssparse/RssparserNew.py -f https://remoteok.io/remote-jobs.rss
-python ~/Dropbox/rssparse/rssparse/RssparserNew.py -f https://weworkremotely.com/categories/1/jobs.rss
-python ~/Dropbox/rssparse/rssparse/RssparserNew.py -f https://dribbble.com/jobs.rss?location=Anywhere
-python ~/Dropbox/rssparse/rssparse/RssparserNew.py -f https://jobspresso.co/?feed=job_feed&job_types=developer&search_location&job_categories&search_keywords
-python ~/Dropbox/rssparse/rssparse/RssparserNew.py -f https://weworkremotely.com/categories/2-programming/jobs.rss
-python ~/Dropbox/rssparse/rssparse/RssparserNew.py -f https://jobs.github.com/positions.atom
-python ~/Dropbox/rssparse/rssparse/RssparserNew.py -f http://feeds.feedblitz.com/flexjobs
-python ~/Dropbox/rssparse/rssparse/RssparserNew.py -f http://careers.stackoverflow.com/jobs/feed?allowsremote=True
+python3 RssparserNew.py -f http://python.org.ar/trabajo/rss
+python3 RssparserNew.py -f https://remoteok.io/remote-jobs.rss
+python3 RssparserNew.py -f https://dribbble.com/jobs.rss?location=Anywhere
+python3 RssparserNew.py -f https://jobspresso.co/?feed=job_feed&job_types=developer&search_location&job_categories&search_keywords
+python3 RssparserNew.py -f https://rss.indeed.com/rss?q=development
+python3 RssparserNew.py -f https://rss.indeed.com/rss?q=javascript
+python3 RssparserNew.py -f https://rss.indeed.com/rss?q=react 
+python3 RssparserNew.py -f https://rss.indeed.com/rss?q=python
+python3 RssparserNew.py -f https://rss.indeed.com/rss?q=engineer
+python3 RssparserNew.py -f https://rss.indeed.com/rss?q=developer
+python3 RssparserNew.py -f https://rss.indeed.com/rss?q=devops
+python3 RssparserNew.py -f https://rss.indeed.com/rss?q=devs 
+python3 RssparserNew.py -f https://www.remotepython.com/latest/jobs/feed/
+python3 RssparserNew.py -f https://jobs.adafruit.com/wpjobboard/xml/rss/?query=&amp;location=&amp;type%5B%5D=7%22
+python3 RssparserNew.py -f https://www.golangprojects.com/rss.xml
+python3 RssparserNew.py -f https://hasjob.co/feed
+python3 RssparserNew.py -f https://www.krop.com/services/feeds/rss/latest/
+python3 RssparserNew.py -f https://www.djangoproject.com/rss/community/jobs/
+
+
 
 
