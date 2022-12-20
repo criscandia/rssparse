4d247a535c9afd0521570ab145726d0aadb52e47
Cristian Candia <candiacristian@gmail.com>
2022-12-20 10:50:03 -0300

    Cron for country filter added

---
+++ cronrsseu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Activate virtualenv
+
+cd ~/Documents/Programs/Projects/rssparse
+
+source env/bin/activate
+
+#Execute script rssparse
+
+python3 RssparserByCountry.py -f https://app.vuejobs.com/feed/posts
+python3 RssparserByCountry.py -f https://landing.jobs/feed?remote=true
+python3 RssparserByCountry.py -f https://weworkremotely.com/categories/remote-full-stack-programming-jobs.rss
+python3 RssparserByCountry.py -f https://weworkremotely.com/categories/remote-back-end-programming-jobs.rss
+python3 RssparserByCountry.py -f https://weworkremotely.com/categories/remote-front-end-programming-jobs.rss
+python3 RssparserByCountry.py -f https://weworkremotely.com/categories/remote-devops-sysadmin-jobs.rss
+python3 RssparserByCountry.py -f https://weworkremotely.com/categories/remote-full-stack-programming-jobs.rss
+
+
