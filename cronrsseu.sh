e5404ebe66e6139ad81bff02c87dbefa91597100
Cristian Candia <candiacristian@gmail.com>
2022-12-22 12:51:36 -0300

    Gdocs file added

--- cronrsseu.sh
+++ cronrsseu.sh
@@ -8,12 +8,12 @@ source env/bin/activate
 
 #Execute script rssparse
 
-python3 RssparserByCountry.py -f https://app.vuejobs.com/feed/posts
-python3 RssparserByCountry.py -f https://landing.jobs/feed?remote=true
-python3 RssparserByCountry.py -f https://weworkremotely.com/categories/remote-full-stack-programming-jobs.rss
-python3 RssparserByCountry.py -f https://weworkremotely.com/categories/remote-back-end-programming-jobs.rss
-python3 RssparserByCountry.py -f https://weworkremotely.com/categories/remote-front-end-programming-jobs.rss
-python3 RssparserByCountry.py -f https://weworkremotely.com/categories/remote-devops-sysadmin-jobs.rss
-python3 RssparserByCountry.py -f https://weworkremotely.com/categories/remote-full-stack-programming-jobs.rss
+python3 RssparserNewGdocs.py -f https://app.vuejobs.com/feed/posts
+python3 RssparserNewGdocs.py -f https://landing.jobs/feed?remote=true
+python3 RssparserNewGdocs.py -f https://weworkremotely.com/categories/remote-full-stack-programming-jobs.rss
+python3 RssparserNewGdocs.py -f https://weworkremotely.com/categories/remote-back-end-programming-jobs.rss
+python3 RssparserNewGdocs.py -f https://weworkremotely.com/categories/remote-front-end-programming-jobs.rss
+python3 RssparserNewGdocs.py -f https://weworkremotely.com/categories/remote-devops-sysadmin-jobs.rss
+python3 RssparserNewGdocs.py -f https://weworkremotely.com/categories/remote-full-stack-programming-jobs.rss
 
 
